f0e04566b1fa02669a30e5e64749d0146c0158fd
masayukinii1011 <masayukinii1011@gmail.com>
2020-06-26 16:54:13 +0900

    input実装

--- components/Instrument.vue
+++ components/Instrument.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <div>test</div>
+    <div @click="start()">test</div>
+    <div>
+      <input @input="setVolumeAmount($event.target.value)" :value="volumeAmount" :min="volumeAmount" max="0" type="range">
+    </div>
+    <div>
+      <input @input="setFilterAmount($event.target.value)" :value="filterAmount" :min="filterAmount" max="100" type="range">
+    </div>
+    <div>
+      <input @input="setTremoloAmount($event.target.value)" :value="tremoloAmount" :min="tremoloAmount" max="100" type="range">
+    </div>
+    <div>
+      <input @input="setDelayAmount($event.target.value)" :value="delayAmount" :min="delayAmount" max="100" type="range">
+    </div>
+    <div>
+      <input @input="setReverbAmount($event.target.value)" :value="reverbAmount" :min="reverbAmount" max="100" type="range">
+    </div>
   </div>
 </template>
 
@@ -20,6 +35,11 @@ export default {
       tremolo: "",
       delay: "",
       reverb: "",
+      volumeAmount: -100,
+      filterAmount: 0,
+      tremoloAmount: 0,
+      delayAmount: 0,
+      reverbAmount: 0,
     };
   },
   mounted: function() {
@@ -39,34 +59,51 @@ export default {
       });
       */
 
-    this.fliter = new Tone.AutoFilter({
+    this.filter = new Tone.AutoFilter({
       frequency: 0
     }).start();
-    this.fliter.wet.value = 100;
+    this.filter.wet.value = this.filterAmount;
 
     this.tremolo = new Tone.Tremolo({
       frequency: 1,
       depth: 1,
       spread: 0
     }).start();
-    this.tremolo.wet.value = 0;
+    this.tremolo.wet.value = this.tremoloAmount;
 
-    this.delay = new Tone.FeedbackDelay().toMaster()
-    this.delay.wet.value = 0;
+    this.delay = new Tone.FeedbackDelay()
+    this.delay.wet.value = this.delayAmount;
 
     this.reverb = new Tone.Freeverb().toMaster()
-    this.reverb.wet.value = 0;
+    this.reverb.wet.value = this.reverbAmount;
 
     this.player = new Tone.Player({
       url: this.url,
       loop: true,
-      autostart: true,
-    }).chain(this.fliter, this.tremolo, this.delay, this.reverb);
+      //autostart: true,
+    }).chain(this.filter, this.tremolo, this.delay, this.reverb);
 
-    this.player.volume.value = -16;
+    this.player.volume.value = this.volumeAmount;
   },
   methods: {
-
+    start: function(){
+      this.player.start();
+    },
+    setVolumeAmount: function(value){
+      this.player.volume.value = value;
+    },
+    setFilterAmount: function(value){
+      this.filter.wet.value = value;
+    },
+    setTremoloAmount: function(value){
+      this.tremolo.wet.value = value;
+    },
+    setDelayAmount: function(value){
+      this.delay.wet.value = value;
+    },
+    setReverbAmount: function(value){
+      this.reverb.wet.value = value;
+    },
   }
 };
 </script>
