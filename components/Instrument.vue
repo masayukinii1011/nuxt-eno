0c354fbff1d455d2ba799194d1f8f53910061b7c
masayukinii1011 <masayukinii1011@gmail.com>
2020-07-01 00:20:32 +0900

    スタイル適用

--- components/Instrument.vue
+++ components/Instrument.vue
@@ -1,59 +1,67 @@
 <template>
-  <div class="container">
-    <div>{{title}}</div>
-    <div>
-      <span class="input-label">Volume</span>
-      <input
-        @input="setVolumeAmount($event.target.value)"
-        :value="volumeAmount"
-        :min="volumeAmount"
-        max="0"
-        type="range"
-      />
-    </div>
-    <div>
-      <span class="input-label">Filter</span>
-      <input
-        @input="setFilterAmount($event.target.value)"
-        :value="filterAmount"
-        min="0"
-        :max="filterAmount"
-        step="0.1"
-        type="range"
-      />
-    </div>
-    <div>
-      <span class="input-label">Vibrato</span>
-      <input
-        @input="setVibratoAmount($event.target.value)"
-        :value="vibratoAmount"
-        :min="vibratoAmount"
-        max="40"
-        step="0.4"
-        type="range"
-      />
-    </div>
-    <div>
-      <span class="input-label">Tremolo</span>
-      <input
-        @input="setTremoloAmount($event.target.value)"
-        :value="tremoloAmount"
-        :min="tremoloAmount"
-        max="8"
-        step="0.08"
-        type="range"
-      />
-    </div>
-    <div>
-      <span class="input-label">Panner</span>
-      <input
-        @input="setPannerAmount($event.target.value)"
-        :value="pannerAmount"
-        :min="pannerAmount"
-        max="16"
-        step="0.16"
-        type="range"
-      />
+  <div class="instrument-container">
+    <div class="instrument-title">{{title}}</div>
+    <div class="input-container">
+      <div>
+        <span class="input-label">Volume</span>
+        <input
+          @input="setVolumeAmount($event.target.value)"
+          :value="volumeAmount"
+          :min="volumeAmount"
+          max="0"
+          type="range"
+          class="input"
+          step="0.64"
+        />
+      </div>
+      <div>
+        <span class="input-label">Filter</span>
+        <input
+          @input="setFilterAmount($event.target.value)"
+          :value="filterAmount"
+          min="0"
+          :max="filterAmount"
+          step="0.1"
+          type="range"
+          class="input"
+        />
+      </div>
+      <div>
+        <span class="input-label">Vibrato</span>
+        <input
+          @input="setVibratoAmount($event.target.value)"
+          :value="vibratoAmount"
+          :min="vibratoAmount"
+          max="40"
+          step="0.4"
+          type="range"
+          class="input"
+        />
+      </div>
+      <div>
+        <span class="input-label">Tremolo</span>
+        <input
+          @input="setTremoloAmount($event.target.value)"
+          :value="tremoloAmount"
+          :min="tremoloAmount"
+          max="8"
+          step="0.08"
+          type="range"
+          class="input"
+        />
+      </div>
+      <div>
+        <span class="input-label">Panner</span>
+        <input
+          @input="setPannerAmount($event.target.value)"
+          :value="pannerAmount"
+          :min="pannerAmount"
+          max="16"
+          step="0.16"
+          type="range"
+          class="input"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -76,7 +84,7 @@ export default {
       vibrato: "",
       tremolo: "",
       panner: "",
-      volumeAmount: -100,
+      volumeAmount: -64,
       filterAmount: 10,
       vibratoAmount: 0,
       tremoloAmount: 0,
@@ -84,6 +92,7 @@ export default {
     };
   },
   watch: {
+    //バッファへの読み込み監視
     buffer: function() {
       if (!this.loaded) {
         this.loaded = true;
@@ -189,13 +198,36 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.container {
-  margin: 16px 0;
+<style scoped>
+.instrument-container {
+  width: 100%;
+  height: 268px;
+  padding: 24px;
+  border-radius: 16px;
+  border: 1px solid rgba(243, 247, 250, 0.05);
+  box-shadow: 6px 6px 12px #b8b9be, -6px -6px 12px #fff;
+}
+
+.instrument-title {
+  margin-bottom: 12px;
+  font-size: 24px;
+}
+
+.input-container {
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+  height: 80%;
 }
 
 .input-label {
-  width: 60px;
+  width: 16%;
+  font-size: 14px;
   display: inline-block;
+}
+
+.input {
+  width: 80%;
+  cursor: pointer;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,40 @@
 <template>
-  <div>
-    <div v-if="clicked" class="instruments">
-      <div v-show="loadedAll">
-        <div>Mix Your Own Vibes.</div>
-        <Instrument :title="'01'" :url="'sounds/c/nuxt-eno01.mp3'" @loaded="loadEvent()" />
-        <Instrument :title="'02'" :url="'sounds/c/nuxt-eno02.mp3'" @loaded="loadEvent()" />
-        <Instrument :title="'03'" :url="'sounds/c/nuxt-eno03.mp3'" @loaded="loadEvent()" />
-        <Instrument :title="'04'" :url="'sounds/c/nuxt-eno04.mp3'" @loaded="loadEvent()" />
-        <Instrument :title="'05'" :url="'sounds/c/nuxt-eno05.mp3'" @loaded="loadEvent()" />
-        <Instrument :title="'06'" :url="'sounds/c/nuxt-eno06.mp3'" @loaded="loadEvent()" />
-        <Instrument :title="'07'" :url="'sounds/c/nuxt-eno07.mp3'" @loaded="loadEvent()" />
-        <Instrument :title="'08'" :url="'sounds/c/nuxt-eno08.mp3'" @loaded="loadEvent()" />
+  <div class="container">
+    <div v-if="clicked" class="clicked-container">
+      <div v-show="loadedAll" class="loaded-container">
+        <div class="title">Mix Your Own Vibes.</div>
+        <div class="instruments">
+          <div class="instrument">
+            <Instrument :title="'01'" :url="'sounds/c/nuxt-eno01.mp3'" @loaded="loadEvent()" />
+          </div>
+          <div class="instrument">
+            <Instrument :title="'02'" :url="'sounds/c/nuxt-eno02.mp3'" @loaded="loadEvent()" />
+          </div>
+          <div class="instrument">
+            <Instrument :title="'03'" :url="'sounds/c/nuxt-eno03.mp3'" @loaded="loadEvent()" />
+          </div>
+          <div class="instrument">
+            <Instrument :title="'04'" :url="'sounds/c/nuxt-eno04.mp3'" @loaded="loadEvent()" />
+          </div>
+          <div class="instrument">
+            <Instrument :title="'05'" :url="'sounds/c/nuxt-eno05.mp3'" @loaded="loadEvent()" />
+          </div>
+          <div class="instrument">
+            <Instrument :title="'06'" :url="'sounds/c/nuxt-eno06.mp3'" @loaded="loadEvent()" />
+          </div>
+          <div class="instrument">
+            <Instrument :title="'07'" :url="'sounds/c/nuxt-eno07.mp3'" @loaded="loadEvent()" />
+          </div>
+          <div class="instrument">
+            <Instrument :title="'08'" :url="'sounds/c/nuxt-eno08.mp3'" @loaded="loadEvent()" />
+          </div>
+        </div>
       </div>
-      <div v-show="!loadedAll">Loading...</div>
+      <div v-show="!loadedAll" class="loading-text">Loading...</div>
     </div>
-    <div v-else @click="firstClick" class="first-view">Please Click The Window.</div>
+    <div v-else @click="firstClick" class="first-view">
+      <div class="first-view-text">Please Click The Window.</div>
+    </div>
   </div>
 </template>
 
@@ -46,10 +66,52 @@ export default {
 };
 </script>
 
-<style>
-.first-view {
+<style scoped>
+.container {
   width: 100vw;
   height: 100vh;
+}
+
+.title {
+  font-size: 54px;
+  margin: 16px 0 24px;
+}
+
+.clicked-container {
+  width: 100%;
+  height: 100%;
+}
+
+.loaded-container {
+  width: 896px;
+  margin: 0 auto 48px;
+}
+
+.instruments {
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 32px;
+}
+
+.instrument {
+  display: flex;
+  justify-content: center;
+}
+
+.loading-text {
+  font-size: 24px;
+  margin: 8px;
+}
+
+.first-view {
+  width: 100%;
+  height: 100%;
   cursor: pointer;
+}
+
+.first-view-text {
+  font-size: 24px;
+  margin: 8px;
 }
 </style>
